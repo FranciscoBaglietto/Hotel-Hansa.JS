55a57f0984e2f592cdfdc40e811e8d6617ad8cc2
FranciscoBaglietto <fbagliettopiermattei@gmail.com>
2022-07-11 18:27:15 -0300

    Agregando carrito

--- main.js
+++ main.js
@@ -1,97 +1,53 @@
+const carrito = [];
 
+let total = 0;
 
-// habitaciones.push({ id: 4, habitacion: "Cuadruple", precio: 400, disponibilidad: 3 }); //Agregando otro objeto mas con push
+function renderizarHabitaciones() {
+    
+    let tienda = document.getElementById("contenedor-habitaciones");
+    
+    habitaciones.forEach(p =>{
+        
+        let habitacionHTML = `
+        <div>
+        <div class= "card">
+            <img src="$(p.img)" class="card-img-top" alt="Habitacion Doble">
+            <div class="card-body text-center">
+                <h5 class="card-title">$(p.habitacion)</h5>
+                <p>$(p.precio)</p>
+            </div>
+            <button class="btn" onClick="agregarProductoAlCarrito($(p.id))>Añadir al carrito</button>
+        </div>
+        </div>
+        `
+        tienda.innerHTML += habitacionHTML;
+        
 
-// const carrito = [];
-// const precioTotal = () => carrito.reduce((acc, elem) => acc + elem.precio, 0);
+    })
 
-// let habitacionesFiltradas = [];
+};
 
-// //Bienvenida
+renderizarHabitaciones();
 
-// console.log("Bienvenidos a Hotel-Hansa")
+function agregarProductoAlCarrito() {
 
+    let habitacion = habitaciones.find(hab => hab.id === id);
 
-// //pregunta si quiere alquilar
-// let alquiler = prompt("¿Quieres alquilar una Habitación? Escribe si/no");
+    let habitacionEnCarrito = carrito.find(hab => hab.id === id);//si hay habitacione en carrito
 
-// let preguntar = false;
+    if(habitacionEnCarrito){
+        habitacionEnCarrito++
 
-// while (!preguntar) {
-//     if (alquiler == "") {
-//         alert("No seleccionaste ninguna Habitacion");
-//         alquiler = prompt("¿Deaseas alquilar una habitacion?");
-//     }
-//     else {
-//         preguntar = true;
-//     }
-//     alquiler.toLowerCase();
-// }
+    }else{
+        habitacion.cantidad = 1;
+        carrito.push(habitacion);
+    }
 
-// if (alquiler == "si") {
+    renderizarCarrito()
+}
 
-//     let filtrar = prompt("Deseas filtrar por precio? si/no").toLowerCase//filtrar por precio
-
-//     if (filtrar = "si") {
-//         let precio = Number(
-//             prompt("Ingrese el precio que deseas filtrar"));
-
-//         habitacionesFiltradas = [];
-//         habitacionesFiltradas = filtrarPrecio(precio);
-
-//         console.log(habitacionesFiltradas);
-//     } 
-// };
-
-// //elegir Habitacion
-// let eleccionHabitacion = "";
-
-// while (eleccionHabitacion != "no".toLowerCase) {
-
-//     let textoAMostrar = '';
-
-//     for (let i = 0; i < habitacionesFiltradas.length; i++) {
-//         textoAMostrar += `Digite ${i + 1} para alquilar Habitacion ${habitacionesFiltradas[i].habitacion}\n`;
-//     }
-
-
-//     eleccionHabitacion = prompt(`
-//       ¿Que habitaciones deseas Alquilar?
-
-//       Para dejar de alquilar, apreta Cancelar.
-
-//      ${textoAMostrar}`);
-
-//     if (eleccionHabitacion == null) {
-//         console.log('No quisiste alquilar ninguna habitacion. Vuelva pronto, o volve a empzar..')
-//         break;
-//     };
-
-//     if (eleccionHabitacion == "no".toLowerCase) {
-//         alert("Gracias por Visistarnos")
-//     };
-
-//     agregarHabitacionAlCarrito(parseInt(eleccionHabitacion));
-
-// };
-
-
-
-
-
-// function agregarHabitacionAlCarrito(id) {
-//     let habitacion = habitaciones.find(habitacion => habitacion.id === id);
-
-//     carrito.push(habitacion);
-//     console.log(`Este es tu carrito hasta el momento:\n`, carrito);
-//     console.log(precioTotal());
-//     //return habitacion.precio;
-// }
-
-// function filtrarPrecio(precio) {
-//     let filtrados = habitaciones.filter(habitacion => habitacion.precio <= precio);
-//     return filtrados;
-// }
-
-
-
+function renderizarCarrito() {
+    
+    let carritoHTML = document.getElementById("carrito");
+    console.log(carritoHTML)
+}
--- stock.js
+++ stock.js
@@ -3,17 +3,20 @@ const habitaciones = [
         id: 1,
         habitacion: "Simple",  //objeto1
         precio: 100,
+        img: `./../imagenes/habitacionSimple.jpg`,
         disponibilidad: 4
     },
     {
         id: 2,
         habitacion: "Doble", //objeto2
         precio: 200,
+        img: `./../imagenes/habitacionDoble.jpg`,
         disponibilidad: 2
     },
     {
         id: 3,
         habitacion: "Triple",    //objeto3
+        img: `./../imagenes/habitacionTriple.jpg`,
         precio: 300,
         disponibilidad: 1
     }
